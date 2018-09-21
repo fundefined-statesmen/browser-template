b1770dcc42a79ef875f44b53a668d0b798d5138c
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-09-21 13:26:26 -0400

    Remove Troublesome /

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://nozama-ecommerce-smst.herokuapp.com/',
+  production: 'https://nozama-ecommerce-smst.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
